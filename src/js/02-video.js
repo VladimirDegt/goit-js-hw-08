b57172a4471caaf37ac1c42df7a39d746b4b08da
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-04-09 11:57:50 +0300

    add part 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
 
+const refs = {
+  iframe: document.querySelector('iframe'),
+  btn: document.querySelector('.btn-clearlocalStorage'),
+}
+
+const player = new Player(refs.iframe);
+
+const startSecondsVideo = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(startSecondsVideo).then(function(seconds) {
+}).catch(function(error) {
+  switch (error.name) {
+      case 'RangeError':
+          break;
+
+      default:
+          break;
+  }
+});
+
+const onPlay = () => {
+  player.on('timeupdate', (data) => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    });
+};
+
+const throttled = throttle(onPlay, 1000);
+player.on('play', throttled);
+
+
+const onBtnClick = () => {
+  localStorage.removeItem('videoplayer-current-time');
+};
+refs.btn.addEventListener('click', onBtnClick);
