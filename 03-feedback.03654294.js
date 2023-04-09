c32948509e76437c5828d12af5916d037cc3ade7
VladimirDegt <107486166+VladimirDegt@users.noreply.github.com>
2023-04-09 09:51:06 +0000

    Deploying to gh-pages from @ VladimirDegt/goit-js-hw-08@6c8348043761f926b707fd8ce21ed8ac76e5d55b 🚀

---
+++ 03-feedback.03654294.js
@@ -0,0 +1,2 @@
+({button:document.querySelector("button")}).button.addEventListener("submit",(()=>{console.log("ok")}));
+//# sourceMappingURL=03-feedback.03654294.js.map
---
+++ 03-feedback.46dae6a4.js
@@ -0,0 +1,2 @@
+document.querySelector("button").addEventListener("submit",(function(){console.log("ok")}));
+//# sourceMappingURL=03-feedback.46dae6a4.js.map
