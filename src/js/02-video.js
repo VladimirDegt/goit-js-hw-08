7fa031f8417f5c0e76df7ff451828964e196ec1b
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-04-09 16:27:50 +0300

    усі 3 частини

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,25 +1,21 @@
 import Player from '@vimeo/player';
-let throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
 const refs = {
   iframe: document.querySelector('iframe'),
   btn: document.querySelector('.btn-clearlocalStorage'),
-}
+};
 
 const player = new Player(refs.iframe);
 
 const startSecondsVideo = localStorage.getItem('videoplayer-current-time');
 if (startSecondsVideo) {
   player.setCurrentTime(startSecondsVideo);
-};
+}
 
-const onPlay = () => {
-  player.on('timeupdate', throttle((data) => {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
-    }, 1000));
-};
-
-player.on('play', onPlay);
+player.on('timeupdate', throttle((data) => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000));
 
 const onBtnClick = () => {
   localStorage.removeItem('videoplayer-current-time');
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,69 @@
+import throttle from 'lodash.throttle';
+
 const refs = {
-  button: document.querySelector('button'),
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('[name="email"]'),
+  textarea: document.querySelector('[name="message"]'),
 };
 
-const onButtonClick = () => {
-  console.log('ok')
+const STORAGE_KEY = "feedback-form-state";
+const dataObject = {};
+
+// --------------------- функція виводу повідомлень ---------------------------
+const sendMessageForm = (element) => {
+  if (!element.email.value.trim() || !element.message.value.trim()) {
+    if (document.querySelector('form p')) {
+      document.querySelector('form p').remove();
+    }
+    document.querySelector('form').insertAdjacentHTML('afterbegin', '<p style="color: red">Будь ласка заповніть усі поля!</p>');
+  } else {
+    if (document.querySelector('form p')) {
+      document.querySelector('form p').remove();
+    }
+    document.querySelector('form').insertAdjacentHTML('afterbegin', '<p style="color: blue">Повідомлення відправлено</p>');
+}};
+
+// ---------------------слухач сабміту форми ---------------------------
+const onFormSubmit = (event) => {
+  const element = event.target.elements;
+  const formData = new FormData(event.target);
+
+  sendMessageForm(element);
+  event.preventDefault();
+
+  formData.forEach ( (value, name) => {
+    dataObject[name] = value.trim();
+  });
+  console.log(dataObject);
+  event.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+};
+
+// ---------------------слухач інпуту форми ---------------------------
+const onFormInput = (e) => {
+  dataObject[e.target.name] = e.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataObject));
+};
+
+// ---------------------слухач ESC форми -----------------------------
+const onEscClick = (event) => {
+  if (event.code === 'Escape') {
+    document.querySelector('.feedback-form [name="email"]').value = "";
+    document.querySelector('.feedback-form [name="message"]').value = "";
+  }
+};
+
+// --------------------- функція заповнення полей форми ----------------
+const currentObjStorage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+if(currentObjStorage) {
+  refs.email.value = currentObjStorage.email;
+  refs.textarea.value = currentObjStorage.message;
 }
-refs.button.addEventListener('submit', onButtonClick);
+
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+window.addEventListener('keyup', onEscClick);
 
 
 
